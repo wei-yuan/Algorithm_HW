b1b568363dabd3e1a75927d8e95c1ffa18c0453f
paslab <paslab@paslabde-MacBook-Air.local>
2017-03-05 15:26:29 +0800

    recursive version second summit

--- hw1-1_recursive.cpp
+++ hw1-1_recursive.cpp
@@ -1,17 +1,25 @@
 #include <cstdio>
 #include <cstdlib>
+#include <math.h>
 
 int Find_Max_Subarray(int A[n],int low,int high){
-	mid = (low+high)/2;
-	Find_Max_Subarray(A[n],low,mid);  //Left half
-	Find_Max_Subarray(A[n],mid,high); //Right half	
-	Find_Max_Crossing_Subarray(A[n],low,mid,high);
-	//if(left_sum >= mid_sum && left_sum >= right_sum)
-	//	return left_low, left_high, left_sum
-	//if(right_sum >= mid_sum && right_sum >= left_sum)
-	//	return right_low, right_high, right_sum
-	//else
-	//	return mid_low, mid_high, mid_sum
+	if(low == high)
+		printf("%d %d %d",low,low,A[low]);
+	else{
+		mid = floor(low+high/2);
+		Find_Max_Subarray(A[n],low,mid);  //Left half
+		Find_Max_Subarray(A[n],mid,high); //Right half	
+		Find_Max_Crossing_Subarray(A[n],low,mid,high);
+		if(left_sum >= mid_sum && left_sum >= right_sum)
+		//	return left_low, left_high, left_sum
+		//	printf("%d %d %d",left_low,left_high,left_sum);
+		if(right_sum >= mid_sum && right_sum >= left_sum)
+		//	return right_low, right_high, right_sum
+		//	printf("%d %d %d",right_low,right_high,right_sum);
+		else
+		//	return mid_low, mid_high, mid_sum
+		//	printf("%d %d %d",mid_low,mid_high,mid_sum);
+	}
 }
 
 int Find_Max_Crossing_Subarray(int A[n],int low,int mid, int high){
@@ -23,7 +31,7 @@ int Find_Max_Crossing_Subarray(int A[n],int low,int mid, int high){
 		for(int j=i; j<=mid; j++){
 			sum += A[j];
 			if(j == mid)
-				last_term_sum = sum;
+				last_term_sum = sum; // Save data only when j reach mid
 		}	
 		if(last_term_sum > left_sum){
 			left_sum  = last_term_sum;
@@ -38,10 +46,10 @@ int Find_Max_Crossing_Subarray(int A[n],int low,int mid, int high){
 	sum = 0;
 	int right_low, right_high, right_sum = -1000000;
 	for(int i=high;i >= mid; i++){
-		for(int j=i; j>=mid; j--)
+		for(int j=i; j>=mid; j--){
 			sum += A[j];
 			if( j == mid)
-				last_term_sum = sum;
+				last_term_sum = sum; // Save data only when j reach mid
 		}		
 		if(last_term_sum > right_sum){
 			right_sum  = last_term_sum;
@@ -55,6 +63,7 @@ int Find_Max_Crossing_Subarray(int A[n],int low,int mid, int high){
 	mid_high = right_high;
 	mid_sum  = left_sum + right_sum;
 	//return mid_low,mid_high,mid_sum;
+	//printf("%d %d %d",mid_low,mid_high,mid_sum);
 }
 
 int main(){
@@ -68,7 +77,7 @@ int main(){
 		if( n<1 || n >= 100000 ) 
 			break;
 
-		int low,high,flag = 0,minus = 0,sum = 0,max_sum = -1000000;
+		int low,high,flag = 0, minus = 0;
 		int A[n];
 		
 		for(int i=0;i<n;i++){
@@ -83,8 +92,10 @@ int main(){
 		if(flag == 1 || minus == n)
 			break;
 		
+
+
 		//Find Max Subarray: divide and conquer
-		//Find_Max_Subarray(A[n],low,high);
+		Find_Max_Subarray(A[n],low,high);
 	}
 	return 0;
 }
